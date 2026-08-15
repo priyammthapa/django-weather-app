5fa71f6ba04895cce76bfd8fe29475390e50c5c2
Priyam Thapa <priyam.thapa2008@gmail.com>
2026-08-15 17:16:01 +0530

    Update weather app

--- weather/urls.py
+++ weather/urls.py
@@ -3,5 +3,5 @@ from .views import home, weather_api
 
 urlpatterns = [
     path("", home, name="home"),
-    path('api/weather/', weather_api, name="weather_api"),
+    path("api/weather/", weather_api, name="weather_api"),
 ]
--- weather/views.py
+++ weather/views.py
@@ -1,15 +1,54 @@
+import requests
+from django.http import JsonResponse
 from django.shortcuts import render
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 
-# Create your views here.
+
 def home(request):
-    return render(request, "weather/index.html")
+    return render(request, 'weather/index.html')
 
-@api_view(['GET'])
+
 def weather_api(request):
-    return Response({
-        "city": "Kolkata",
-        "temperature": 30,
-        "condition": "Cloudy"
+    city = request.GET.get('city')
+
+    if not city:
+        return JsonResponse({'error': 'Please enter a city'}, status=400)
+
+    geo_url = "https://geocoding-api.open-meteo.com/v1/search"
+
+    geo_params = {
+        'name': city,
+        'count': 1,
+        'language': 'en',
+        'format': 'json'
+    }
+
+    geo_response = requests.get(geo_url, params=geo_params)
+    geo_data = geo_response.json()
+
+    if 'results' not in geo_data:
+        return JsonResponse({'error': 'City not found'}, status=404)
+
+    location = geo_data['results'][0]
+
+    latitude = location['latitude']
+    longitude = location['longitude']
+
+    weather_url = "https://api.open-meteo.com/v1/forecast"
+
+    weather_params = {
+        'latitude': latitude,
+        'longitude': longitude,
+        'current': 'temperature_2m,relative_humidity_2m,weather_code',
+        'timezone': 'auto'
+    }
+
+    weather_response = requests.get(weather_url, params=weather_params)
+    weather_data = weather_response.json()
+
+    return JsonResponse({
+        'city': location['name'],
+        'country': location.get('country'),
+        'temperature': weather_data['current']['temperature_2m'],
+        'humidity': weather_data['current']['relative_humidity_2m'],
+        'weather_code': weather_data['current']['weather_code']
     })
