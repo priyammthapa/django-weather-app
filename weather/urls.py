2e09b10b8d87bd447e890fcf314f7f4b68e5d806
Priyam Thapa <priyam.thapa2008@gmail.com>
2026-08-15 15:01:03 +0530

    Add weather API and fix templates

--- weather/urls.py
+++ weather/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import views
+from .views import home, weather_api
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("", home, name="home"),
+    path('api/weather/', weather_api, name="weather_api"),
 ]
--- weather/views.py
+++ weather/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
 def home(request):
     return render(request, "weather/index.html")
+
+@api_view(['GET'])
+def weather_api(request):
+    return Response({
+        "city": "Kolkata",
+        "temperature": 30,
+        "condition": "Cloudy"
+    })
